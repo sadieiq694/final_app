3bec0ff0c95be6a2cf45d9b89b48aa39bc4b8d60
Sadie Allen <sadie.la418@gmail.com>
2018-04-30 12:04:25 -0400

    New app

---
+++ find_titles.m
@@ -0,0 +1,7 @@
+function title_list = find_titles(data_list)
+    [r c] = size(data_list);
+    title_list = cell(1, r);
+    for i = 1:r
+        title_list{1,i} = get_title(data_list{i, 1});
+    end
+end
