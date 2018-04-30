d5952a8540379ce2aef0588d52ffa27c5744cedb
Sadie Allen <sadie.la418@gmail.com>
2018-04-29 23:56:54 -0400

    help me

--- final_main.m
+++ final_main.m
@@ -36,128 +36,21 @@ search_results = find_by_search(data, ["math", "engineer"]) % works!!
 
 
 
+%%% API FUN TIME %%%
+stuff = webread('https://api.bls.gov/publicAPI/v2/timeseries/data/LNS13000000');
+% this is only for like 2 years >:( how do I get more!?!?!
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% set key and URL
-url = 'http://api.bls.gov/publicAPI/v2/timeseries/data/';
-key = '71b6da5094ad4048aa7c53b9b8958000';
-
-b = bls([], '71b6da5094ad4048aa7c53b9b8958000');
-
-%data = fetch(b, 'PRS85006092', 'catalog', 'true');
-c = b;
-s = 'PRS85006092';
-
-% this is literally just setting constant values
-BLS_RESPONSE_SUCCESS = 'REQUEST_SUCCEEDED';
-BLS_RESPONSE_CATALOG_FAIL = 'unable to get catalog data';
-% this creates a cell array of four words why do we need this
-validCatalogTrue = {'true','on'};
-validCatalogFalse = {'false','off'};
-validCatalog = {validCatalogTrue{:}, validCatalogFalse{:}};
-% these lines set the default to the past 10 years 
-defaultCatalog = 0;
-defaultEndYear = datestr(now(), 'yyyy');
-defaultStartYear = num2str(str2double(defaultEndYear)-9);
-% this is a function that checks if a year entered by the user is valid
-validationYear = ...
-  @(x) (ischar(x) && regexp(x, '\d\d\d\d')) || ...
-       (isnumeric(x) && isscalar(x) && (x>=1900)); 
-% function that checks if a catalog is valid (1 or 0)
-validationCatalog = ...
-    @(x) (ischar(x) && any(strcmpi(x, validCatalog))) || ...
-         (isnumeric(x) && isscalar(x));
-% function that checks if a series is valid :P
-validationSeries = ...
-  @(x) ischar(x) || iscellstr(x);
-% creates an inputParser object
-p = inputParser();
-addRequired(p, 's', validationSeries); % adds parameter s to parsers 
-addOptional(p, 'startyear', defaultStartYear, validationYear); %adds year params
-addOptional(p, 'endyear', defaultEndYear, validationYear);
-addParameter(p, 'catalog', defaultCatalog, validationCatalog); % adds catalog params
-parse(p, s, 'catalog', 'true'); %parses stuff??? Not sure
-%sets start year and end year
-startYear = p.Results.startyear;
-endYear = p.Results.endyear;
-% not really sure what this does... looks like it is checking for a valid
-% catalog
-if ischar(p.Results.catalog) && ...
-  any(strcmpi(p.Results.catalog, validCatalogTrue))
-  catalog = 1;
-elseif isnumeric(p.Results.catalog) && p.Results.catalog > 0
-  catalog = 1;
-else
-  catalog = 0;
+values = [0];
+for i = 1:length(stuff.Results.series.data)
+    values(i) = str2num(stuff.Results.series.data{i}.value);
 end
 
-% BLS specifies uppercase series.
-if ischar(s)
-s = cellstr(s);
-end
-s = upper(s);
+API_data = stuff.Results.series.data;
+% data from Jan 2016 - March 2018 
 
-% Stup request and payload.
-url = c.url;
-options = weboptions('MediaType','application/json');
-dates = {'startyear', startYear, 'endyear', endYear};
-params = {'catalog', catalog};
+months = 1:27;
 
-% Try registration key
-if ~isempty(c.key)
-  auth = {'registrationKey',c.key};
-else
-  auth = {};
-end
-
-% so.... we would just add more series ids? modify code to do this!?!?!
-% write a damn function 
-data = struct('seriesid',{s}, ...
-            dates{:}, ...
-            params{:}, ...
-            auth{:});
-
-% Submit POST request to BLS.
-  try
-    response = webwrite(url, data, options);
-  catch err
-    error('Error connecting to BLS servers.');
-  end
-
-  % Response okay?
-  if ~strcmpi(response.status,BLS_RESPONSE_SUCCESS)
-    warning('Request failed with message ''%s''',response.message{:});
-    d.SeriesID = [];
-    d.Data = [];
-    return;
-  end
+plot(months, values)
 
 
 
@@ -173,29 +66,6 @@ data = struct('seriesid',{s}, ...
 
 
 
-% set web options, dates??
-options = weboptions('MediaType','application/json'); % set mediatype to application/json
-options = weboptions('MediaType','application/x-www-form-urlencoded'); % set mediatype to application/json
-% maybe specify dates later? figure out how to access data first :P
-% dates = {'startyear', startYear, 'endyear', endYear}; % startYear, endYear variables
-
-% get data??? I hope???
-response = webread(url, 'seriesid','LAUCN040010000000005', options);
 
 
 
-%Set up request and payload.
-  
-  params = {'catalog', catalog};
-  
-  data = struct('seriesid',{s}, ...
-                dates{:}, ...
-                params{:}, ...
-                auth{:});
-            
-  % Submit POST request to BLS.
-  try
-    response = webwrite(url, data, options);
-  catch err
-    error('Error connecting to BLS servers.');
-  end
